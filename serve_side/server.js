e831dac1b9c7988f64a4b176747dc9bb6e11bfe5
007hyno <Parasrawat05@gmail.com>
2022-12-18 13:48:51 +0530

    remove logs

--- serve_side/server.js
+++ serve_side/server.js
@@ -2,12 +2,10 @@ const express = require('express')
 const app = express()
 app.use(express.json()) //app can accept json
 const bcrypt = require('bcrypt') //for crypting user password
-// const { restart } = require('nodemon');
 const port = 3001 //port number
 
 const fs = require('firebase-admin');
 const serviceAccount = require('./creds.json');
-// const { response } = require('express');
 
 fs.initializeApp({
     credential: fs.credential.cert(serviceAccount)
@@ -19,8 +17,6 @@ const db = fs.firestore(); //database instence
 app.post('/api/register', async(req, res) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000")
 
-  res.send({"email":"asa"})
-  console.log("datas: "+req.body.email);
     try {
       console.log(req.body);
       const id = req.body.email;
@@ -32,9 +28,9 @@ app.post('/api/register', async(req, res) => {
         firstName: req.body.firstName,
         lastName: req.body.lastName,
       };
-      const usersDb = db.collection('users'); 
-      const response = await usersDb.doc(id).set(userJson);
-      res.status(201).send(response);
+      const usersDb = db.collection('users'); //storing db reference
+      const response = await usersDb.doc(id).set(userJson); //inserting doc 
+      res.status(201).send(response);//response
     } catch(error) {
       res.send(error);
     }
@@ -42,7 +38,7 @@ app.post('/api/register', async(req, res) => {
   
 
 //Home 
-app.get('/:email',async(req,res)=>{
+app.get('/:email',async(req,res)=>{//home page route with get 
   res.header("Access-Control-Allow-Origin", "http://localhost:3000")
 
     let email_ = req.params.email;
@@ -52,26 +48,23 @@ app.get('/:email',async(req,res)=>{
 })
 app.get('/',async(req,res)=>{
   res.header("Access-Control-Allow-Origin", "http://localhost:3000")
-    res.send("response.data()");
+    res.send("Testing home api");
 })
 
 //user login 
 
 app.post('/api/login',async(req,res)=>{
   res.header("Access-Control-Allow-Origin", "http://localhost:3000")
-
-  res.send("req.body")
-        console.log("data - : "+JSON.stringify(req.body));
         try {
-          const userRef = db.collection("users").doc(req.body.email);
+          const userRef = db.collection("users").doc(req.body.email);//taking collectoin reference
           const response = await userRef.get();
-          if (!response.exists) {
+          if (!response.exists) { //if user email dosn't exist
             return res.status(404).send('No such document!');
           } 
           try{
               if(await bcrypt.compare(req.body.password,response.data().password)){//comp password
                   res.send('Success : '+response.data().email);
-                // res.redirect("/"); 
+                // res.redirect("/home") // if success redirect to home page 
                   res.send(response.data());
               }else{
                   res.send("passoword miss match")
