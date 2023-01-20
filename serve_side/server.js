09ec09dcd4c50d279d9e0544d590714338c54132
007hyno <Parasrawat05@gmail.com>
2023-01-20 13:04:22 +0530

    Hide some keys

--- serve_side/server.js
+++ serve_side/server.js
@@ -106,5 +106,5 @@ app.post('/api/login',async(req,res)=>{
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-  console.log(process.env);
+  // console.log(process.env);
 })
